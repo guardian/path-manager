8749c00e990b49fb24110a1225551c006f3e64a2
Stephen Wells <stephen.wells@guardian.co.uk>
2015-02-24 15:15:05 +0000

    initial commit of url-manager

---
+++ project/UrlManagerBuild.scala
@@ -0,0 +1,36 @@
+import com.gu.deploy.PlayArtifact._
+import sbt._
+import sbt.Keys._
+import play.Play.autoImport._
+import PlayKeys._
+import com.typesafe.sbt.web._
+
+object UrlManagerBuild extends Build {
+
+  val commonSettings =
+    Seq(
+      scalaVersion := "2.11.4",
+      scalaVersion in ThisBuild := "2.11.4",
+      organization := "com.gu",
+      version      := "0.1",
+      fork in Test := false,
+      resolvers ++= Seq("Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"),
+      scalacOptions ++= Seq("-feature", "-deprecation", "-language:higherKinds", "-Xfatal-warnings"),
+      doc in Compile <<= target.map(_ / "none"),
+      incOptions := incOptions.value.withNameHashing(nameHashing = true)
+    )
+
+  val root = Project("url-manager", file(".")).enablePlugins(play.PlayScala).enablePlugins(SbtWeb)
+    .settings(libraryDependencies += ws)
+    .settings(commonSettings ++ playArtifactDistSettings ++ playArtifactSettings: _*)
+    .settings(magentaPackageName := "url-manager")
+
+  def playArtifactSettings = Seq(
+    ivyXML :=
+      <dependencies>
+        <exclude org="commons-logging"/>
+        <exclude org="org.springframework"/>
+        <exclude org="org.scala-tools.sbt"/>
+      </dependencies>
+  )
+}
---
+++ project/plugins.sbt
@@ -0,0 +1,8 @@
+// The Typesafe repository
+resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
+
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.3.6")
+
+addSbtPlugin("com.typesafe.sbt" % "sbt-web" % "1.0.0")
+
+addSbtPlugin("com.eed3si9n" % "sbt-buildinfo" % "0.3.2")
