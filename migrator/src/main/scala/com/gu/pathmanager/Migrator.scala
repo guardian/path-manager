5fa6c117621376587b31256c9957c2041311dbf1
Stephen Wells <stephen.wells@guardian.co.uk>
2015-04-09 11:42:11 +0100

    connects to the database

--- migrator/src/main/scala/com/gu/pathmanager/Migrator.scala
+++ migrator/src/main/scala/com/gu/pathmanager/Migrator.scala
@@ -1,13 +1,49 @@
 package com.gu.pathmanager
 
 import java.io.{FileNotFoundException, FileInputStream}
+import java.sql.DriverManager
 import java.util.Properties
+import javax.sql.DataSource
+import oracle.jdbc.pool.OracleDataSource
+import scalikejdbc._
+
+
+class Migrator(conf: DbConfiguration) {
+
+  ConnectionPool.singleton(new DataSourceConnectionPool(dataSource(conf)))
+
+  def doSomeSql = DB readOnly { implicit session =>
+    sql"select count(*) as c from page_draft".map(rs => rs.long("c")).single().apply().get
+  }
+
+
+  private def dataSource(conf: DbConfiguration): DataSource = {
+    val oracleDataSource = new OracleDataSource
+
+    oracleDataSource.setUser(conf.user)
+    oracleDataSource.setPassword(conf.password)
+    oracleDataSource.setDriverType("thin")
+    oracleDataSource.setServerName(conf.dbAddress)
+    oracleDataSource.setPortNumber(1521)
+    oracleDataSource.setServiceName(conf.dbService)
+
+    val properties = new Properties
+    properties.put("v$session.program", "path-manager-migrator")
+    oracleDataSource.setConnectionProperties(properties)
+
+
+    println(s"Connecting as ${conf.user} to ${conf.dbAddress} / ${conf.dbService}")
+    oracleDataSource
+  }
+}
 
 
 object Migrator {
 
   def main(args: Array[String]) {
-    println(loadProperties)
+    val migrator = new Migrator(loadProperties)
+
+    println(s"there are ${migrator.doSomeSql} draft pages")
 
   }
 
@@ -24,7 +60,8 @@ object Migrator {
     }
 
     DbConfiguration(
-      url = readProperty("url"),
+      dbAddress = readProperty("databaseAddress"),
+      dbService = readProperty("databaseService"),
       user = readProperty("user"),
       password = readProperty("password")
     )
@@ -37,4 +74,4 @@ object Migrator {
   }
 }
 
-case class DbConfiguration(url: String, user: String, password: String)
+case class DbConfiguration(dbAddress: String, dbService: String, user: String, password: String)
